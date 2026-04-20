1219325ddc27ffd0bb5a9f7d6bbb235e35b56e53
Jonathan Ruppel <jonathan.ruppel@yahoo.com>
2026-04-20 15:13:58 -0500

    move to JS

---
+++ js/app.js
@@ -0,0 +1,41 @@
+// Create map centered on the US
+const map = L.map('map').setView([37.8, -96], 4);
+
+// Base map tiles
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 18,
+  attribution: '© OpenStreetMap'
+}).addTo(map);
+
+// GeoJSON for US states
+const geojsonUrl =
+  "https://eric.clst.org/assets/wiki/uploads/Stuff/gz_2010_us_040_00_500k.json";
+
+// Simple static style
+function style(feature) {
+  return {
+    fillColor: "#4a90e2",
+    weight: 1,
+    color: "white",
+    fillOpacity: 0.5
+  };
+}
+
+// Load and draw GeoJSON
+fetch(geojsonUrl)
+  .then(res => res.json())
+  .then(data => {
+    L.geoJSON(data, {
+      style: style
+    }).addTo(map);
+  });
+
+fetch("../data/teams.json")
+  .then(res => res.json())
+  .then(points => {
+    points.forEach(p => {
+      L.marker([p.lat, p.lon])
+        .addTo(map)
+        .bindPopup(p.team);
+    });
+  });
