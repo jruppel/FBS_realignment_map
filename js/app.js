e499c8b9c63a355235e825a9683caf8fa0496a11
Jonathan Ruppel <jonathan.ruppel@yahoo.com>
2026-04-23 16:51:31 -0500

    updated map, teams, and points styles

--- js/app.js
+++ js/app.js
@@ -2,9 +2,10 @@
 const map = L.map('map').setView([37.8, -96], 4);
 
 // Base map tiles
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  maxZoom: 18,
-  attribution: '© OpenStreetMap'
+L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
+  maxZoom: 19,
+  subdomains: 'abcd',
+  attribution: '© OpenStreetMap © CARTO'
 }).addTo(map);
 
 // GeoJSON for US states
@@ -14,10 +15,10 @@ const geojsonUrl =
 // Simple static style
 function style(feature) {
   return {
-    fillColor: "#ffffff",
-    weight: .1,
-    color: "black",
-    fillOpacity: 0.5
+    fillColor: "#1f1f1f",
+    weight: 1,
+    color: "#3a3a3a",
+    fillOpacity: 0.75
   };
 }
 
@@ -34,8 +35,31 @@ fetch("data/teams.json")
   .then(res => res.json())
   .then(points => {
     points.forEach(p => {
-      L.marker([p.lat, p.lon])
+      const icon = L.divIcon({
+          className: "",
+          iconSize: [34, 34],
+          iconAnchor: [17, 0],
+          html: `
+          <div style="
+          width:45px;
+          height:45px;
+          border-radius:50%;
+          background:${p.primary};
+          border:4px solid ${p.secondary};
+          display:flex;
+          align-items:center;
+          justify-content:center;
+          font-size:10px;
+          font-weight:900;
+          color:#ffffff;
+          ">
+          ${p.abbr}
+          </div>
+          `
+      });
+
+      L.marker([p.lat, p.lon], { icon })
         .addTo(map)
-        .bindPopup(p.team);
+        .bindPopup(`<b>${p.team}</b>`);
     });
   });
